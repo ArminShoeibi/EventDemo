8f4ff0943dfdd49a0aa072e210ce67459459d054
Armin Shoeibi <68565441+ArminShoeibi@users.noreply.github.com>
2021-05-13 16:57:46 +0430

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EventDemo
+{
+    public class Employee
+    {
+
+        private DateTime _birthDate;
+        public event Action OnBirthday;
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthDate;
+            }
+            set
+            {
+                _birthDate = value;
+                if (_birthDate.Date == DateTime.Today)
+                {
+                    OnBirthday += Employee_OnBirthday;
+                    OnBirthday();
+                }
+
+            }
+        }
+
+        private void Employee_OnBirthday()
+        {
+            Console.WriteLine("Happy Birthday");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EventDemo
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new();
+            employee.BirthDate = DateTime.Now;
+            var ea = employee.BirthDate;
+
+            Console.ReadLine();
+        }
+    }
+}
